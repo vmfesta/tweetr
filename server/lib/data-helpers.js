8bdf03b145e6dd26f3661d32232fab4e25fa264d
vmfesta <vmfesta@gmail.com>
2017-07-13 12:18:18 -0400

    updated application to work with mongoDB, save and get tweets are now consuming the database instead of json file

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -6,22 +6,24 @@ const simulateDelay = require("./util/simulate-delay");
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
   return {
-
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
+      db.collection("tweets").insertOne(newTweet, (err, result) => {
+        if (err) {
+          return callback(err);
+        }
         callback(null, true);
       });
     },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
+      var objects = db.collection("tweets").find().toArray((err, tweets) => {
+        if (err) {
+          return callback(err);
+        }
+        callback(null, tweets);
       });
     }
-
   };
-}
+};
